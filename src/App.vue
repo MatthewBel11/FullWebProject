59f4fe3d7a59f4b5a8b2975f7a92be15eb06e6f4
c2026706 <c2026706@ncl.ac.uk>
2024-03-21 21:06:58 +0000

    - Removed navbar from App.vue and made it into a reuseable component

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,47 @@
 <template>
-  <div class="navbar">
-    <nav>
-      <div class="navitem">
-        <RouterLink to="/Login">Login</RouterLink>
-      </div>
-      <div class="navitem">
-        <RouterLink to="/About">About</RouterLink>
-      </div>
-    </nav>
+  <!-- This loads the content for HOME page for single page application -->
+  <div class="hidden" v-show="$route.path === '/'">
+    <div class="background-home">
+      <NavView />
+    </div>
+    <TestView />
   </div>
 
-  <RouterView />
+  <!-- This loads the content for ABOUT page for single page application -->
+  <div v-show="$route.path === '/about'">
+    <div class="background-about">
+      <NavView />
+    </div>
+  </div>
 </template>
 
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import NavView from '../src/views/NavView.vue'
+import TestView from '../src/views/TestView.vue'
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+const currentRoute = route.path
 </script>
 
 <style scoped>
-.navbar {
-  background-color: yellow;
-  width: 100%;
+.hidden {
   margin: 0;
-  height: 50px;
+  padding: 0;
 }
 
-.navitem {
-  display: inline-block;
-  margin-right: 10px;
-  text-decoration: none;
+.background-home,
+.background-about {
+  margin: 0;
+  width: 100%;
+  height: 100vh;
+}
+
+.background-home {
+  background-image: url('../src/assets/images/1.jpg');
+}
+
+.background-about {
+  background-image: url('../src/assets/images/image.jfif');
 }
 </style>
---
+++ src/views/NavView.vue
@@ -0,0 +1,93 @@
+<template>
+  <header class="header">
+    <div class="menu-icon" @click="toggleNavbar">
+      <div class="line"></div>
+      <div class="line"></div>
+      <div class="line"></div>
+    </div>
+    <div class="navbar">
+      <nav class="nav">
+        <div class="navitem">
+          <RouterLink to="/" @click="toggleNavbar">HOME</RouterLink>
+        </div>
+        <div class="navitem">
+          <RouterLink to="/login" @click="toggleNavbar">LOGIN</RouterLink>
+        </div>
+        <div class="navitem">
+          <RouterLink to="/about" @click="toggleNavbar">ABOUT</RouterLink>
+        </div>
+      </nav>
+    </div>
+    <div class="logo-container">
+      <img class="logo" src="C:\Users\44730\FullWeb\src\assets\images\logo.png" />
+    </div>
+  </header>
+  <RouterView />
+</template>
+
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import { ref } from 'vue'
+</script>
+
+<style scoped>
+@font-face {
+  font-family: 'Seb Thin';
+  src: url('C:\Users\44730\FullWeb\src\assets\fonts\SebneueThin-a3GK.otf');
+}
+
+.header {
+  display: flex;
+  flex-direction: row; /* Align children in a row */ /* Spread children apart */
+  align-items: flex-start; /* Align items at the top */
+  padding: 10px;
+  background-color: rgba(0, 0, 0, 0);
+}
+
+.menu-icon {
+  cursor: pointer;
+  padding: 10px;
+}
+
+.line {
+  width: 25px;
+  height: 3px;
+  background-color: #ffffff;
+  margin: 5px 0;
+  margin-right: 20px;
+  border-radius: 2px;
+}
+
+.logo-container {
+  margin-left: auto; /* Align logo to the end */
+}
+
+.navbar {
+  display: flex;
+}
+
+.logo {
+  padding: 20px;
+  height: 150px;
+  object-fit: contain;
+}
+
+.nav {
+  display: flex;
+  flex-direction: row; /* Stack nav items horizontally */
+}
+
+.navitem {
+  margin-left: 20px;
+  margin-top: 15px;
+  margin-right: 20px; /* Add spacing between nav items */
+}
+
+.navitem a {
+  color: #ffffff; /* Set text color */
+  text-decoration: none;
+  font-weight: bold;
+  font-size: 16px;
+  font-family: 'Seb Thin', sans-serif;
+}
+</style>
