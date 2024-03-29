1508bf85a24c3e8d1c076f61a502a3648c2d63a3
c2026706 <c2026706@ncl.ac.uk>
2024-03-29 20:07:44 +0000

    - Added slide out menu and hamburger icon to navbar

--- src/views/NavView.vue
+++ src/views/NavView.vue
@@ -1,25 +1,39 @@
 <template>
   <header class="header">
-    <div class="menu-icon" @click="toggleNavbar">
+    <div v-if="!isNavbarOpen" class="menu-icon" @click="toggleNavbar">
       <div class="line"></div>
       <div class="line"></div>
       <div class="line"></div>
     </div>
+
+    <!-- Rotated menu icon -->
+    <div v-if="isNavbarOpen" class="menu-icon rotated" @click="toggleNavbar">
+      <div class="rotated-line"></div>
+      <div class="rotated-line"></div>
+      <div class="rotated-line"></div>
+    </div>
+
+    <div class="menu-slide" :class="{ 'menu-slide-active': isNavbarOpen }">
+      <!-- Content for the sliding menu -->
+    </div>
+
     <div class="navbar">
       <nav class="nav">
         <div class="navitem">
-          <RouterLink to="/" @click="toggleNavbar">HOME</RouterLink>
+          <RouterLink to="/">HOME</RouterLink>
         </div>
         <div class="navitem">
-          <RouterLink to="/login" @click="toggleNavbar">LOGIN</RouterLink>
+          <RouterLink to="/login">LOGIN</RouterLink>
         </div>
         <div class="navitem">
-          <RouterLink to="/about" @click="toggleNavbar">ABOUT</RouterLink>
+          <RouterLink to="/about">ABOUT</RouterLink>
         </div>
       </nav>
     </div>
     <div class="logo-container">
-      <img class="logo" src="C:\Users\44730\FullWeb\src\assets\images\logo.png" />
+      <RouterLink to="/">
+        <img class="logo" src="C:\Users\44730\FullWeb\src\assets\images\logo.png" />
+      </RouterLink>
     </div>
   </header>
   <RouterView />
@@ -28,12 +42,20 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import { ref } from 'vue'
+
+const isNavbarOpen = ref(false)
+
+const toggleNavbar = () => {
+  isNavbarOpen.value = !isNavbarOpen.value
+}
 </script>
 
 <style scoped>
+/* Your existing styles */
+
 @font-face {
   font-family: 'Seb Thin';
-  src: url('C:\Users\44730\FullWeb\src\assets\fonts\SebneueThin-a3GK.otf');
+  src: url('../src/assets/fonts/SebneueThin-a3GK.otf'); /* Modern Browsers */
 }
 
 .header {
@@ -42,20 +64,6 @@ import { ref } from 'vue'
   align-items: flex-start; /* Align items at the top */
   padding: 10px;
   background-color: rgba(0, 0, 0, 0);
-}
-
-.menu-icon {
-  cursor: pointer;
-  padding: 10px;
-}
-
-.line {
-  width: 25px;
-  height: 3px;
-  background-color: #ffffff;
-  margin: 5px 0;
-  margin-right: 20px;
-  border-radius: 2px;
 }
 
 .logo-container {
@@ -78,7 +86,7 @@ import { ref } from 'vue'
 }
 
 .navitem {
-  margin-left: 20px;
+  margin-left: 5px;
   margin-top: 15px;
   margin-right: 20px; /* Add spacing between nav items */
 }
@@ -89,5 +97,76 @@ import { ref } from 'vue'
   font-weight: bold;
   font-size: 16px;
   font-family: 'Seb Thin', sans-serif;
+}
+
+.menu-icon {
+  margin-top: 9px;
+  margin-left: 10px;
+}
+
+.menu-slide {
+  position: fixed;
+  top: 0;
+  left: -300px; /* Offscreen initially */
+  width: 300px;
+  height: 100%;
+  background-color: #333; /* Adjust background color as needed */
+  transition: left 0.2s ease-in-out; /* Smooth transition for sliding */
+  z-index: 1; /* Ensure sliding menu is behind menu icon */
+}
+
+.menu-slide-active {
+  left: 0; /* Slide in when active */
+}
+
+.menu-icon .line {
+  width: 28px;
+  height: 3px;
+  background-color: #ffffff;
+  margin: 5px 0;
+  margin-right: 20px;
+  border-radius: 2px;
+}
+
+.menu-icon:active .line:nth-child(1) {
+  transform-origin: 75% 75%;
+  transform: rotate(-45deg);
+}
+
+.menu-icon:active .line:nth-child(2) {
+  width: 0px;
+}
+
+.menu-icon:active .line:nth-child(3) {
+  transform-origin: 100% 100%;
+  transform: rotate(45deg);
+}
+
+.menu-icon.rotated {
+  position: relative;
+  z-index: 2;
+}
+
+.rotated .rotated-line {
+  width: 28px;
+  height: 3px;
+  background-color: #ffffff;
+  margin: 5px 0;
+  margin-right: 20px;
+  border-radius: 2px;
+}
+
+.rotated .rotated-line:nth-child(1) {
+  transform-origin: 75% 75%;
+  transform: rotate(-45deg);
+}
+
+.rotated .rotated-line:nth-child(2) {
+  width: 0px;
+}
+
+.rotated .rotated-line:nth-child(3) {
+  transform-origin: 100% 100%;
+  transform: rotate(45deg);
 }
 </style>
